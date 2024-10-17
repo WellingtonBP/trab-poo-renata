49cd2db517170cce72e37ff0e6cd287fe564b48e
Wellington Pacheco <wellingtonpacheco14@gmail.com>
2024-10-17 19:29:25 -0300

    feat: add token validation

--- src/main/java/iff/poo/application/resources/UserResource.java
+++ src/main/java/iff/poo/application/resources/UserResource.java
@@ -2,13 +2,15 @@ package iff.poo.application.resources;
 
 import iff.poo.application.dto.UserDto;
 import iff.poo.core.user.UserService;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.jboss.logging.Logger;
 
 import java.util.Map;
@@ -16,11 +18,15 @@ import java.util.Map;
 @Path("/user")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@RequestScoped
 public class UserResource {
     private static final Logger LOG = Logger.getLogger(UserResource.class);
 
     @Inject
     UserService userService;
+
+    @Inject
+    JsonWebToken jwt;
 
     @POST
     public Response createUser(UserDto userDto) {
@@ -31,5 +37,12 @@ public class UserResource {
             LOG.error(ex);
             return Response.serverError().build();
         }
+    }
+
+    @GET
+    @Path("/teste-role")
+    @RolesAllowed("traveler")
+    public Response testeUser(@Context SecurityContext ctx) {
+        return Response.ok().entity(Map.of("Hello", ctx.getUserPrincipal() != null ? ctx.getUserPrincipal().getName() : "World")).build();
     }
 }
